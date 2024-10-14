04a60e666bea06f415d4be5f38fdbd1a1c084959
olegultra <147877661+olegultra@users.noreply.github.com>
2024-10-14 11:13:47 +0300

    Update pre.js

--- pre.js
+++ pre.js
@@ -145,7 +145,7 @@ body {
             },
             {
                 type: "text", text: "Button Text",
-                x: 144, y: 455,
+                x: 142, y: 455,
                 width: 200,
                 classes: ["TNO-header-text"], style: "font-size: 18px; text-align: center; text-shadow: 1px 0 1px #000000, 0 1px 1px #000000, -1px 0 1px #000000, 0 -1px 1px #000000;",
                 replaceable: true
